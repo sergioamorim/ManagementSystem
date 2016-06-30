fadc066275c724db9aea09b41c68afcc445c6861
sergio <sergioatamorim@gmail.com>
2016-06-30 02:53:02 -0300

    constructor fixed

--- src/Researcher.java
+++ src/Researcher.java
@@ -1,6 +1,6 @@
 
 public class Researcher extends AbleToAskResource {
-	public Researcher(int id, String name, boolean allocationInProgress){
-		super(id, name, allocationInProgress);
+	public Researcher(int id, String name, String email){
+		super(id, name, email);
 	}
 }
