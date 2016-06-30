59c7631a9c4e491a95abb2b35716060059f96f30
sergio <sergioatamorim@gmail.com>
2016-06-30 02:56:12 -0300

    fixed finishAllocation method and constructor added

--- src/Administrator.java
+++ src/Administrator.java
@@ -1,7 +1,11 @@
 
 public class Administrator extends AbleToAskResource {
+	public Administrator(int id, String name, String email) {
+		super(id, name, email);
+	}
+
 	public void finishAllocation(Allocation allocation) {
-		if (allocation.getActivity().description != null) {
+		if (allocation.getActivity().getDescription() != null) {
 			allocation.getResponsible().saveResourceInHistory(allocation);
 			allocation.getResponsible().saveActivityResourceInHistory(allocation.getActivity());
 			allocation.getResponsible().setAllocationInProgress(false);
